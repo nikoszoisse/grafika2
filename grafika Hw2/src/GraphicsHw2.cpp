b22ac2c8fabe06f6c9ce28ac27e7c370c6162a6e
nikoszoisse <aloha5568>
2015-01-05 04:33:45 +0200

    CAmera setted up

--- grafika Hw2/src/GraphicsHw2.cpp
+++ grafika Hw2/src/GraphicsHw2.cpp
@@ -9,6 +9,7 @@
 #include<iostream>
 #include <vector>
 #include <string>
+#include <math.h>
 #include "./objects/Object.h"
 #include "./objects/Cube.h"
 #include "./objects/Character.h"
@@ -16,52 +17,94 @@
 
 using namespace std;
 
-Character *player;
-	//static vector<Cube*> cubes;
+	Character *player;
 	int grid_size=64;
 	LightObject sun;
 	vector<Cube*> cubes;
 	float sizeOfCube=0.5;
-	//METHODS
-GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
-  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
-  {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
 
+	// angle of rotation for the camera direction
+	float angle = 0.0f;
 
-void createField(){
-	//cubes.push_back(new Cube(sizeOfCube,0,0,0));
-	for(int i=0;i<grid_size;i++){
-		for(int j=0;j<grid_size;j++){
-				cubes.push_back(new Cube(sizeOfCube,-i*(sizeOfCube+0.01),0,-j*sizeOfCube));
-				cubes.push_back(new Cube(sizeOfCube,i*(sizeOfCube+0.01),0,-j*sizeOfCube));
+	// actual vector representing the camera's direction
+	float lx=0.0f,lz=-1.0f;
+
+	// XZ position of the camera
+	float x=0.0f, z=5.0f;
+
+	// the key states. These variables will be zero
+	//when no key is being presses
+	float deltaAngle = 0.0f;
+	float deltaMove = 0;
+	int xOrigin = -1;
+
+	void mouseMove(int x, int y) {
+
+		// this will only be true when the left button is down
+		if (xOrigin >= 0) {
+
+			// update deltaAngle
+			deltaAngle = (x - xOrigin) * 0.001f;
+
+			// update camera's direction
+			lx = sin(angle + deltaAngle);
+			lz = -cos(angle + deltaAngle);
+			//Rediplay or glRotafef() the panel
+			cout << lx << lz << endl;
+			glutPostRedisplay();
 		}
 	}
 
-}
+	void mouseButton(int button, int state, int x, int y) {
 
-void reshapeScene(int w,int h){
+		// only start motion if the left button is pressed
+		if (button == GLUT_LEFT_BUTTON) {
 
-		// Prevent a divide by zero, when window is too short
-		// (you cant make a window of zero width).
-		if(h == 0)
-			h = 1;
-		float ratio = 1.0* w / h;
+			// when the button is released
+			if (state == GLUT_UP) {
+				angle += deltaAngle;
+				xOrigin = -1;
+			}
+			else  {// state = GLUT_DOWN
+				xOrigin = x;
+			}
+		}
+	}
 
-		// Use the Projection Matrix
-		glMatrixMode(GL_PROJECTION);
+	void createField(){
+		//cubes.push_back(new Cube(sizeOfCube,0,0,0));
+		for(int i=0;i<grid_size;i++){
+			for(int j=0;j<grid_size;j++){
+					cubes.push_back(new Cube(sizeOfCube,-i*(sizeOfCube+0.01),0,-j*sizeOfCube));
+					//cubes.push_back(new Cube(sizeOfCube,i*(sizeOfCube+0.01),0,-j*sizeOfCube));
+			}
+		}
+
+	}
+
+	void reshapeScene(int w,int h){
+
+			// Prevent a divide by zero, when window is too short
+			// (you cant make a window of zero width).
+			if(h == 0)
+				h = 1;
+			float ratio = 1.0* w / h;
+
+			// Use the Projection Matrix
+			glMatrixMode(GL_PROJECTION);
 
 			// Reset Matrix
-		glLoadIdentity();
+			glLoadIdentity();
 
-		// Set the viewport to be the entire window
-		glViewport(0, 0, w, h);
+			// Set the viewport to be the entire window
+			glViewport(0, 0, w, h);
 
-		// Set the correct perspective.
-		gluPerspective(45,ratio,1,1000);
+			// Set the correct perspective.
+			gluPerspective(45,ratio,1,1000);
 
-		// Get Back to the Modelview
-		glMatrixMode(GL_MODELVIEW);
-	}
+			// Get Back to the Modelview
+			glMatrixMode(GL_MODELVIEW);
+		}
 
 	void renderScene(){
 		//Clear Color and Depth Buffers
@@ -71,30 +114,20 @@ void reshapeScene(int w,int h){
 		glLoadIdentity();
 
 		// Set the camera
-		gluLookAt(	0, 1.0f, 1,
-					0,1,0,
-					0.0f, 1.0f,  0.0f);
+		gluLookAt(	x, 1.0f, z,
+				x+lx, 1.0f,  z+lz,
+				0.0f, 1.0f,  0.0f);
 
-	        // Draw ground
-
-		int i;
-		int j;
-
+		// Draw the GROUND
 		for (vector<Cube*>::iterator it = cubes.begin() ; it != cubes.end(); ++it){
 			(*it)->view();
 		}
 		cout << cubes.size() << endl;
-		//glTranslatef(0.0,0.0,sizeOfCube1);
-		//drawCube(sizeOfCube);
-		/*glTranslatef(-3.2f, 0.0f, 0.0f);
-		glColor3f(1.0,1.0,1.0);
-		glutWireCube(sizeOfCubef);
-		glTranslatef(sizeOfCubef, 0.0f, 0.0f);
-		glutWireCube(sizeOfCubef);*/
 
 
 		glutSwapBuffers();
 	}
+
 	void init(int argc,char** argv){
 		glutInit(&argc, argv);
 		glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
@@ -102,6 +135,9 @@ void reshapeScene(int w,int h){
 		glutInitWindowSize(500, 500);
 		glutCreateWindow("Something Like Minecraft");
 		createField();
+		//Mouse LIsteners
+		glutMouseFunc(mouseButton);
+		glutMotionFunc(mouseMove);
 
 		glutDisplayFunc(renderScene);
 		glutReshapeFunc(reshapeScene);
@@ -111,8 +147,8 @@ void reshapeScene(int w,int h){
 
 	}
 
-int main(int argc,char** argv) {
-	init(argc,argv);
+	int main(int argc,char** argv) {
+		init(argc,argv);
 
-	return EXIT_SUCCESS;
-}
+		return EXIT_SUCCESS;
+	}
